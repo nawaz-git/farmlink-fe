527a1cdba77ff64891c46132c495eaeca3c44a63
Nawaz Pasha <nawaz22022001@gmail.com>
2023-01-29 10:29:38 +0530

    Completed Orders Module Customer

--- src/app/home/orders/orders.component.ts
+++ src/app/home/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent {
+  status: any = {
+    Pending: false,
+    Pick: false,
+    Completed: false
+  }
+  selectedStatus: any;
+  orders: any
+  constructor(private api: HttpService) {
 
+  }
+  statusChange() {
+    let selectedStatus;
+    for (let stat in this.status) {
+      if (this.status[stat]) {
+        selectedStatus = stat;
+        this.status[stat] = false
+      }
+
+    }
+    this.selectedStatus = selectedStatus
+    console.log(selectedStatus);
+    this.getOrders()
+  }
+
+  getOrders() {
+    let data = {
+      userId: localStorage.getItem('userId'),
+      status: this.selectedStatus
+    }
+    this.api.getOrdersCustomer(data).subscribe((res: any) => {
+      this.orders = res
+      console.log(this.orders);
+
+    })
+  }
 }
